348220c233a81056788874cbb240f44d97782129
Shi Lei <shi_lei@massclouds.com>
2024-05-13 14:27:08 +0000

    fix

--- top_earlycon/src/lib.rs
+++ top_earlycon/src/lib.rs
@@ -6,7 +6,7 @@ use core::panic::PanicInfo;
 #[no_mangle]
 pub extern "Rust" fn runtime_main(_cpu_id: usize, _dtb_pa: usize) {
     let msg = "\n[early_console]: Hello, ArceOS!\n";
-    early_console::write_bytes(msg.as_bytes());
+    earlycon::write_bytes(msg.as_bytes());
 }
 
 pub fn panic(info: &PanicInfo) -> ! {
